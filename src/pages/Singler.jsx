c4a3251ded66872c3ddb054f56fbae08b3c3307b
SahuNaman <namansahu191102@gmail.com>
2024-07-09 11:17:50 +0530

    nji

--- src/pages/Singler.jsx
+++ src/pages/Singler.jsx
@@ -11,7 +11,7 @@ const Singler = () => {
             <div className='col-md-6 '><img src={location.state.recipe.image}/></div>
             <div className='col-md-6 '><h3>Title : {location.state.recipe.label}</h3>
             <h4>MealType : {location.state.recipe.mealType}</h4>
-            <div className='m-auto'><Link to={location.state.recipe.url} className='btn btn-info'>How to Make</Link><div/>
+            <div className='d-flex justify-content-center mt-3'><Link to={location.state.recipe.url} className='btn btn-primary'>How to make</Link></div>
             </div>
         </div>
         <div className='row'>
